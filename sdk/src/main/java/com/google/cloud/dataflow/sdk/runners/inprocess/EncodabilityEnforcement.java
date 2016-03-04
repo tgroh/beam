1ba3bd18aa07147233b9fcc02bb82dbbeab47fd9
Thomas Groh <tgroh@google.com>
2016-03-04 08:53:50 -0800

    Add ModelEnforcements
    ModelEnforcements ensure that user-written code conforms to the model,
    in order to ensure that it is portable between runners.
    
    Add EncodabilityEnforcement and ImmutabilityEnforcement.
    EncodabilityEnforcement ensures that all values can be encoded.
    ImmutabilityEnforcement ensures that values are not mutated within the
    processing of a Bundle.

---
+++ sdk/src/main/java/com/google/cloud/dataflow/sdk/runners/inprocess/EncodabilityEnforcement.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.dataflow.sdk.runners.inprocess;
+
+import com.google.cloud.dataflow.sdk.coders.Coder;
+import com.google.cloud.dataflow.sdk.coders.CoderException;
+import com.google.cloud.dataflow.sdk.runners.inprocess.InProcessPipelineRunner.CommittedBundle;
+import com.google.cloud.dataflow.sdk.transforms.AppliedPTransform;
+import com.google.cloud.dataflow.sdk.util.CoderUtils;
+import com.google.cloud.dataflow.sdk.util.WindowedValue;
+import com.google.cloud.dataflow.sdk.values.PCollection;
+import com.google.common.base.Throwables;
+
+/**
+ * Enforces that all elements in a {@link PCollection} can be encoded using that
+ * {@link PCollection PCollection's} {@link Coder}.
+ */
+class EncodabilityEnforcmentFactory implements ModelEnforcementFactory {
+  @Override
+  public <T> ModelEnforcement<T> forBundle(
+      CommittedBundle<T> input, AppliedPTransform<?, ?, ?> consumer) {
+    return new EncodabilityEnforcement<>(input);
+  }
+
+  public static class EncodabilityEnforcement<T> extends AbstractModelEnforcement<T> {
+    private Coder<T> coder;
+
+    public EncodabilityEnforcement(CommittedBundle<T> input) {
+      coder = input.getPCollection().getCoder();
+    }
+
+    @Override
+    public void beforeElement(WindowedValue<T> element) {
+      try {
+        CoderUtils.clone(coder, element.getValue());
+      } catch (CoderException e) {
+        throw Throwables.propagate(e);
+      }
+    }
+
+    @Override
+    public void afterFinish(CommittedBundle<T> input, InProcessTransformResult result) {}
+  }
+}
