6f9a5decbc9c1c87fb8fc059f321479b02f51952
Thomas Groh <tgroh@google.com>
2016-07-13 14:40:46 -0700

    Dynamically choose number of shards in the DirectRunner
    Do so by obtaining a count of the elements and obtaining the number of
    shards based on the number of outputs.
    
    WIP: Can't express a dynamically sharded write in terms of a write.

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/WriteWithShardingFactory.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/WriteWithShardingFactory.java
@@ -19,9 +19,25 @@
 package org.apache.beam.runners.direct;
 
 import org.apache.beam.sdk.io.Write;
+import org.apache.beam.sdk.io.Write.Bound;
+import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.PTransform;
+import org.apache.beam.sdk.transforms.ParDo;
+import org.apache.beam.sdk.transforms.Values;
+import org.apache.beam.sdk.transforms.windowing.DefaultTrigger;
+import org.apache.beam.sdk.transforms.windowing.GlobalWindows;
+import org.apache.beam.sdk.transforms.windowing.Window;
+import org.apache.beam.sdk.values.KV;
+import org.apache.beam.sdk.values.PCollection;
+import org.apache.beam.sdk.values.PCollectionView;
+import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.PInput;
 import org.apache.beam.sdk.values.POutput;
+
+import org.joda.time.Duration;
 
 import java.util.concurrent.ThreadLocalRandom;
 
@@ -37,12 +53,67 @@ class WriteWithShardingFactory implements PTransformOverrideFactory {
     if (transform instanceof Write.Bound) {
       Write.Bound<InputT> that = (Write.Bound<InputT>) transform;
       if (that.getNumShards() == 0) {
-        PTransform<InputT, OutputT> shardControlledWrite =
-            (PTransform<InputT, OutputT>)
-                that.withNumShards(3 + ThreadLocalRandom.current().nextInt(5));
-        return shardControlledWrite;
+        return (PTransform<InputT, OutputT>) new DynamicallyReshardedWrite<InputT>(that);
       }
     }
     return transform;
+  }
+
+  private static class DynamicallyReshardedWrite <T> extends PTransform<PCollection<T>, PDone> {
+    private final transient Write.Bound<T> original;
+
+    private DynamicallyReshardedWrite(Bound<T> original) {
+      this.original = original;
+    }
+
+    @Override
+    public PDone apply(PCollection<T> input) {
+      final PCollectionView<Long> numRecords =
+          input
+              .apply(
+                  Window.<T>into(new GlobalWindows())
+                      .triggering(DefaultTrigger.of())
+                      .withAllowedLateness(Duration.ZERO)
+                      .discardingFiredPanes())
+              .apply(Count.<T>globally().asSingletonView());
+      DoFn<T, KV<Long, T>> fn = new ReshardBasedOnCountFn<>(numRecords);
+      PCollection<T> resharded =
+          input
+              .apply(
+                  "ApplySharding",
+                  ParDo.withSideInputs(numRecords)
+                      .of(fn))
+              .apply("GroupIntoShards", GroupByKey.<Long, T>create())
+              .apply(Values.<Iterable<T>>create())
+              .apply(Flatten.<T>iterables());
+      return resharded.apply(original);
+    }
+  }
+
+  private static class ReshardBasedOnCountFn<T> extends DoFn<T, KV<Long, T>> {
+    private final PCollectionView<Long> numRecords;
+    private long currentShard;
+    private long maxShards;
+
+    private ReshardBasedOnCountFn(PCollectionView<Long> numRecords) {
+      this.numRecords = numRecords;
+    }
+
+    @Override
+    public void processElement(ProcessContext c) throws Exception {
+      long count = c.sideInput(numRecords);
+      if (maxShards == 0L) {
+        maxShards = calculateShards(count);
+        currentShard = ThreadLocalRandom.current().nextLong(maxShards);
+      }
+      long shard = currentShard;
+      currentShard = (currentShard + 1) % maxShards;
+      c.output(KV.of(shard, c.element()));
+    }
+
+    private long calculateShards(long totalRecords) {
+      return Math.min(
+          3L + ThreadLocalRandom.current().nextLong(4L), Math.max(totalRecords / 1000, 1L));
+    }
   }
 }
