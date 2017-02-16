ab01c37cfd81cbfceb2fa4ebbe3eacff09a00005
Thomas Groh <tgroh@google.com>
2017-02-15 19:05:08 -0800

    WIP: Flink Surgery

--- runners/flink/runner/src/main/java/org/apache/beam/runners/flink/FlinkRunner.java
+++ runners/flink/runner/src/main/java/org/apache/beam/runners/flink/FlinkRunner.java
@@ -31,6 +31,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
+import org.apache.beam.runners.core.construction.PTransformMatchers;
+import org.apache.beam.runners.core.construction.SingletonInputsAndOutputsOverrideFactory;
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.coders.Coder;
@@ -39,6 +41,8 @@ import org.apache.beam.sdk.coders.KvCoder;
 import org.apache.beam.sdk.coders.ListCoder;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsValidator;
+import org.apache.beam.sdk.runners.PTransformMatcher;
+import org.apache.beam.sdk.runners.PTransformOverrideFactory;
 import org.apache.beam.sdk.runners.PipelineRunner;
 import org.apache.beam.sdk.runners.TransformHierarchy;
 import org.apache.beam.sdk.transforms.Combine;
@@ -46,6 +50,7 @@ import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.transforms.View;
+import org.apache.beam.sdk.transforms.View.AsMap;
 import org.apache.beam.sdk.util.InstanceBuilder;
 import org.apache.beam.sdk.util.PCollectionViews;
 import org.apache.beam.sdk.values.KV;
@@ -117,10 +122,17 @@ public class FlinkRunner extends PipelineRunner<PipelineResult> {
     this.options = options;
     this.ptransformViewsWithNonDeterministicKeyCoders = new HashSet<>();
 
+    // TODO: Surgery
+    ImmutableMap.Builder<PTransformMatcher, PTransformOverrideFactory> matcherOverrides =
+        ImmutableMap.builder();
     ImmutableMap.Builder<Class<?>, Class<?>> builder = ImmutableMap.<Class<?>, Class<?>>builder();
     if (options.isStreaming()) {
-      builder.put(Combine.GloballyAsSingletonView.class,
-          StreamingCombineGloballyAsSingletonView.class);
+      builder.put(
+          Combine.GloballyAsSingletonView.class, StreamingCombineGloballyAsSingletonView.class);
+      matcherOverrides.put(
+          PTransformMatchers.classEqualTo(View.AsMap.class),
+          new StreamingViewAsMapOverrideFactory(this));
+
       builder.put(View.AsMap.class, StreamingViewAsMap.class);
       builder.put(View.AsMultimap.class, StreamingViewAsMultimap.class);
       builder.put(View.AsSingleton.class, StreamingViewAsSingleton.class);
@@ -290,6 +302,22 @@ public class FlinkRunner extends PipelineRunner<PipelineResult> {
 
 
   /////////////////////////////////////////////////////////////////////////////
+
+  private static class StreamingViewAsMapOverrideFactory<K, V>
+      extends SingletonInputsAndOutputsOverrideFactory<
+          PCollection<KV<K, V>>, PCollectionView<Map<K, V>>, View.AsMap<K, V>> {
+    private final FlinkRunner runner;
+
+    private StreamingViewAsMapOverrideFactory(FlinkRunner runner) {
+      this.runner = runner;
+    }
+
+    @Override
+    public PTransform<PCollection<KV<K, V>>, PCollectionView<Map<K, V>>> getReplacementTransform(
+        AsMap<K, V> transform) {
+      return new StreamingViewAsMap<>(runner, transform);
+    }
+  }
 
   /**
    * Specialized implementation for
