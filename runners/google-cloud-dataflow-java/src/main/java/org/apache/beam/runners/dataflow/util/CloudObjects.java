8ab1bd636f346cc710d920a1ec85c1569f9d35ef
Thomas Groh <tgroh@google.com>
2017-04-24 15:54:17 -0700

    fixup! Add Cloud Object Translators for Coders

--- runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/util/CloudObjects.java
+++ runners/google-cloud-dataflow-java/src/main/java/org/apache/beam/runners/dataflow/util/CloudObjects.java
@@ -18,19 +18,11 @@
 
 package org.apache.beam.runners.dataflow.util;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
 import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.Lists;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import org.apache.beam.sdk.coders.Coder;
 import org.apache.beam.sdk.coders.CustomCoder;
-import org.apache.beam.sdk.coders.StandardCoder;
 import org.apache.beam.sdk.util.CloudObject;
-import org.apache.beam.sdk.util.PropertyNames;
 import org.apache.beam.sdk.util.SerializableUtils;
 import org.apache.beam.sdk.util.StringUtils;
 import org.apache.beam.sdk.util.Structs;
@@ -51,9 +43,10 @@ public class CloudObjects {
   }
 
   public static CloudObject asCloudObject(Coder<?> coder) {
-    if (CODER_TRANSLATORS.containsKey(coder.getClass())) {
-      // TODO: Make this cast less dangerous
-      return standardCoderAsCloudObject((StandardCoder<?>) coder);
+    CloudObjectTranslator<Coder<?>> translator =
+        (CloudObjectTranslator<Coder<?>>) CODER_TRANSLATORS.get(coder.getClass());
+    if (translator != null) {
+      return translator.toCloudObject(coder);
     } else if (coder instanceof CustomCoder) {
       return customCoderAsCloudObject((CustomCoder<?>) coder);
     }
@@ -70,38 +63,6 @@ public class CloudObjects {
         "serialized_coder",
         StringUtils.byteArrayToJsonString(SerializableUtils.serializeToByteArray(coder)));
 
-    return result;
-  }
-
-  /**
-   * Convert the provided {@link StandardCoder} to a {@link CloudObject}.
-   */
-  private static CloudObject standardCoderAsCloudObject(StandardCoder<?> coder) {
-    CloudObject result;
-    CloudObjectTranslator<StandardCoder<?>> initializer =
-        (CloudObjectTranslator<StandardCoder<?>>) CODER_TRANSLATORS.get(coder);
-    result = initializer.toCloudObject(coder);
-
-    List<? extends Coder<?>> components = coder.getComponents();
-    if (!components.isEmpty()) {
-      List<CloudObject> cloudComponents = new ArrayList<>(components.size());
-      for (Coder<?> component : components) {
-        cloudComponents.add(asCloudObject(component));
-      }
-      Structs.addList(result, PropertyNames.COMPONENT_ENCODINGS, cloudComponents);
-    }
-
-    String encodingId = coder.getEncodingId();
-    checkNotNull(encodingId, "Coder.getEncodingId() must not return null.");
-    if (!encodingId.isEmpty()) {
-      Structs.addString(result, PropertyNames.ENCODING_ID, encodingId);
-    }
-
-    Collection<String> allowedEncodings = coder.getAllowedEncodings();
-    if (!allowedEncodings.isEmpty()) {
-      Structs.addStringList(
-          result, PropertyNames.ALLOWED_ENCODINGS, Lists.newArrayList(allowedEncodings));
-    }
     return result;
   }
 
