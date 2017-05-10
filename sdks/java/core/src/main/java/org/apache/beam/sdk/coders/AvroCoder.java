4247b440c98fa4114d4d266361ec410ff66bc5b7
Thomas Groh <tgroh@google.com>
2017-05-10 08:56:54 -0700

    Update SDK Coders to return the Empty List from getCoderArguments
    Brings existing Coders in line with the documentation

--- sdks/java/core/src/main/java/org/apache/beam/sdk/coders/AvroCoder.java
+++ sdks/java/core/src/main/java/org/apache/beam/sdk/coders/AvroCoder.java
@@ -26,6 +26,7 @@ import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -320,7 +321,7 @@ public class AvroCoder<T> extends CustomCoder<T> {
 
   @Override
   public List<? extends Coder<?>> getCoderArguments() {
-    return null;
+    return Collections.emptyList();
   }
 
   /**
