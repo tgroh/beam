81e261663170bf57d9484faa8f9d62afa2100b8e
Thomas Groh <tgroh@google.com>
2017-11-22 13:20:55 -0800

    fixup! Migrate MonitorRunnable into QuiescenceDriver

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/DoFnLifecycleManager.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/DoFnLifecycleManager.java
@@ -66,7 +66,7 @@ class DoFnLifecycleManager {
     outstanding.invalidate(currentThread);
     // Block until the invalidate is fully completed
     outstanding.cleanUp();
-    // Remove to try too avoid reporting the same teardown exception twice. May still double-failed,
+    // Remove to try too avoid reporting the same teardown exception twice. May still double-report,
     // but the second will be suppressed.
     Exception thrown = thrownOnTeardown.remove(currentThread);
     if (thrown != null) {
