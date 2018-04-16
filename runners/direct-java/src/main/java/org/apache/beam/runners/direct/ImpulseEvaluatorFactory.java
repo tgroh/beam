e4bd7c01eaa1954896e4a4d7862be58d88e7c2f7
Thomas Groh <tgroh@google.com>
2018-04-16 12:55:44 -0700

    Implement BundleFactory in EvaluationContext
    Use that interface instead of `EvaluationContext` in Impulse and
    GroupByKeyOnly evaluators. These evaluators will be reused in both the
    portable and native runners.

--- runners/direct-java/src/main/java/org/apache/beam/runners/direct/ImpulseEvaluatorFactory.java
+++ runners/direct-java/src/main/java/org/apache/beam/runners/direct/ImpulseEvaluatorFactory.java
@@ -32,9 +32,9 @@ import org.apache.beam.sdk.values.PCollection;
 
 /** The evaluator for the {@link Impulse} transform. Produces only empty byte arrays. */
 class ImpulseEvaluatorFactory implements TransformEvaluatorFactory<AppliedPTransform<?, ?, ?>> {
-  private final EvaluationContext ctxt;
+  private final BundleFactory ctxt;
 
-  ImpulseEvaluatorFactory(EvaluationContext ctxt) {
+  ImpulseEvaluatorFactory(BundleFactory ctxt) {
     this.ctxt = ctxt;
   }
 
@@ -51,13 +51,13 @@ class ImpulseEvaluatorFactory implements TransformEvaluatorFactory<AppliedPTrans
   }
 
   private static class ImpulseEvaluator implements TransformEvaluator<ImpulseShard> {
-    private final EvaluationContext ctxt;
+    private final BundleFactory bundleFactory;
     private final AppliedPTransform<?, PCollection<byte[]>, Impulse> transform;
     private final StepTransformResult.Builder<ImpulseShard> result;
 
     private ImpulseEvaluator(
-        EvaluationContext ctxt, AppliedPTransform<?, PCollection<byte[]>, Impulse> transform) {
-      this.ctxt = ctxt;
+        BundleFactory bundleFactory, AppliedPTransform<?, PCollection<byte[]>, Impulse> transform) {
+      this.bundleFactory = bundleFactory;
       this.transform = transform;
       this.result = StepTransformResult.withoutHold(transform);
     }
@@ -67,7 +67,9 @@ class ImpulseEvaluatorFactory implements TransformEvaluatorFactory<AppliedPTrans
       PCollection<byte[]> outputPCollection =
           (PCollection<byte[]>) Iterables.getOnlyElement(transform.getOutputs().values());
       result.addOutput(
-          ctxt.createBundle(outputPCollection).add(WindowedValue.valueInGlobalWindow(new byte[0])));
+          bundleFactory
+              .createBundle(outputPCollection)
+              .add(WindowedValue.valueInGlobalWindow(new byte[0])));
     }
 
     @Override
